554bfa95eb1f91b1067a523f071d6fc58922cb27
unknown <honorpurpose@gmail.com>
2019-04-15 16:26:11 -0600

    completed 04152019

---
+++ sumofmixedarray.js
@@ -0,0 +1,13 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x){
+    return x.reduce((rt, ce) => {return rt + Number(ce)}, 0)
+}
+
+// best answer
+
+function sumMix(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+  }
