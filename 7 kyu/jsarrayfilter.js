b654f7dc909fbf2d85cd78d9eac729a4face7c3b
unknown <honorpurpose@gmail.com>
2019-04-20 09:26:32 -0600

    completed 04202019

---
+++ 7 kyu/jsarrayfilter.js
@@ -0,0 +1,17 @@
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter((e, i) => e % 2 === 0)
+  }
+
+// best answer 
+
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(function(n){
+      return n % 2 == 0;
+    });
+  }
