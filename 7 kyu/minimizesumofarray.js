4c6464fcd8b1b33020389a40205f5014251dd14b
Brockston King <brockstonking@205-185-99-20.directcom.com>
2019-07-26 17:34:34 -0600

    completed 072619

---
+++ 7 kyu/minimizesumofarray.js
@@ -0,0 +1,38 @@
+// Introduction and Warm-up (Highly recommended)
+// Playing With Lists/Arrays Series
+// Task
+// Given an array of intgers , Find the minimum sum which is obtained from summing each Two integers product .
+
+// Notes
+// Array/list will contain positives only .
+// Array/list will always has even size
+// Input >> Output Examples
+// minSum({5,4,2,3}) ==> return (22) 
+// Explanation:
+// The minimum sum obtained from summing each two integers product , 5*2 + 3*4 = 22
+// minSum({12,6,10,26,3,24}) ==> return (342)
+// Explanation:
+// The minimum sum obtained from summing each two integers product , 26*3 + 24*6 + 12*10 = 342
+// minSum({9,2,8,7,5,4,0,6}) ==> return (74)
+// Explanation:
+// The minimum sum obtained from summing each two integers product , 9*0 + 8*2 +7*4 +6*5 = 74
+
+const minSum = (arr) => {
+    return arr.sort( (a, b) => a-b).reduce( (r, e, i, a) => {
+        r.push(e * a[a.length - 1 - i])
+        return r;
+    }, []).reduce( (r, e, i, a) => {
+        if (i < a.length/2) {
+            r += e
+        }
+        return r;
+    }, 0);
+}
+
+// best answer
+
+function minSum(arr) {
+    return arr.sort( (a,b) => a-b )
+              .slice(0, arr.length/2)
+              .reduce( (acc,curr,index) => acc += curr * arr[ arr.length - index - 1 ], 0 )
+  }
