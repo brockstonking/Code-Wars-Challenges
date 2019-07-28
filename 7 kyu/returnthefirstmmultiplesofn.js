e983c79b1ea3d5171978e5d32df5af66697adefc
Brockston King <brockston.king@gmail.com>
2019-07-28 15:37:25 -0600

    completed 072719

---
+++ 7 kyu/returnthefirstmmultiplesofn.js
@@ -0,0 +1,18 @@
+// Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+// Ex.
+
+// multiples(3, 5)
+// should return
+
+// [5, 10, 15]
+
+const multiples = (m, n) => {
+    let multiples = [];
+    for (let i = 1; multiples.length < m; i++) {
+        if (i % n === 0) {
+            multiples.push(i)
+        }
+    };
+    return multiples;
+}
