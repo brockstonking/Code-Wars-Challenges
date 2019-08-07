ee526f7134029eeea2a4f48421ca367335e69dd7
Brockston King <brockston.king@gmail.com>
2019-08-07 08:14:18 -0600

    completed 080719

--- custom/saltAnalyses.js
+++ custom/saltAnalyses.js
@@ -1,6 +1,10 @@
 const salt = (num1, num2, num3) => {
-    console.log(`Total salts: ${((num1 + num2 + num3)/3)*.0072}`);
-    console.log(`Shred Salts ${ (((num1 + num2 + num3)/3)*.0072) - .35 }`);
+    return [
+        `Total salts: ${Math.round((((num1 + num2 + num3)/3)*.0072) * 10) / 10}`,
+        `Shred Salts ${ (Math.round((((num1 + num2 + num3)/3)*.0072) - .35) * 10) / 10 }`
+    ].map( e => {
+        console.log(e);
+    })
 }
 
-console.log(salt(578, 587, 579))
+console.log(salt(674, 685, 691))
