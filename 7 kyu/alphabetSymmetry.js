48802d544835b93a0a1d51ad43f6b599f807e1a6
Brockston King <brockston.king@gmail.com>
2019-08-13 05:54:38 -0600

    completed 081419

--- 7 kyu/alphabetSymmetry.js
+++ 7 kyu/alphabetSymmetry.js
@@ -9,9 +9,8 @@
 const solve = (arr) => {
     let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     return arr.reduce( (r, e, i) => {
-        r.push(e.split('').reduce( (ru, el, ind, arr) => {
-            let letter = el.toLowerCase();
-            if (arr.indexOf(letter) === alphabet.indexOf(letter)) {
+        r.push(e.toLowerCase().split('').reduce( (ru, el, ind, arr) => {
+            if (arr.indexOf(el) === alphabet.indexOf(el)) {
                 ru++
             }
             return ru;
